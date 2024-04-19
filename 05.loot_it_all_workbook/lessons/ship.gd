3974afa3c72a2aed39e40c31021b70b8db371c9b
JuliaLui642 <159198251+JuliaLui642@users.noreply.github.com>
2024-04-19 15:26:13 -0700

    M5 L6.P1

--- 05.loot_it_all_workbook/lessons/ship.gd
+++ 05.loot_it_all_workbook/lessons/ship.gd
@@ -1,21 +1,27 @@
 extends Area2D
 
-var health = 40
+
 var max_speed := 1200.0
 var velocity := Vector2(0, 0)
 var steering_factor := 3.0
+var health := 10
+var gem_count := 0
+
+func _ready() -> void:
+	area_entered.connect(_on_area_entered)
+	
+	set_health(health +10)
+	
 
 
 func _process(delta: float) -> void:
 	var direction := Vector2(0, 0)
 	direction.x = Input.get_axis("move_left", "move_right")
 	direction.y = Input.get_axis("move_up", "move_down")
-	
 
 	if direction.length() > 1.0:
 		direction = direction.normalized()
 
-	
 	var desired_velocity := max_speed * direction
 	var steering := desired_velocity - velocity
 	velocity += steering * steering_factor * delta
@@ -23,19 +29,17 @@ func _process(delta: float) -> void:
 
 	if velocity.length() > 0.0:
 		get_node("Sprite2D").rotation = velocity.angle()
-
+		
 func set_health(new_health: int) -> void:
 	health = new_health
 	get_node("UI/HealthBar").value = health
-
-func _ready()-> void:
-	area_entered.connect(_on_area_entered)
-	# This call updates the health bar to match the health variable when the
-	# game starts.
-	set_health(health)
-
-
-func _on_area_entered(area_that_entered: Area2D) -> void:
-	set_health(health+ 20)
 	
- 
+func _on_area_entered(area_that_entered: Area2D) -> void:
+	if area_that_entered.is_in_group("gem"):
+		set_gem_count(gem_count + 1)
+	elif area_that_entered.is_in_group("healing_item"):
+		set_health(health + 10)
+		
+func set_gem_count(new_gem_count: int) -> void:
+	gem_count = new_gem_count
+	get_node("UI/GemCount").text = "x" + str(gem_count)
