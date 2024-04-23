2bb4e539e59d4ba43b976e8b9c302c910b568d0f
JuliaLui642 <159198251+JuliaLui642@users.noreply.github.com>
2024-04-23 13:18:32 -0700

    M5. Lesson 10

--- 05.loot_it_all_workbook/lessons/item.gd
+++ 05.loot_it_all_workbook/lessons/item.gd
@@ -4,6 +4,11 @@ extends Area2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	area_entered.connect(_on_area_entered)
+	var tween := create_tween()
+	var sprite_2d := get_node("Sprite2D")
+	var target_position := Vector2(500, 400)
+	var animation_duration := 2.0
+	tween.tween_property(sprite_2d, "position", target_position, animation_duration)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
