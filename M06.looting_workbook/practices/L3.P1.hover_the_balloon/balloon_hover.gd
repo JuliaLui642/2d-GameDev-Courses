80f6530428aaf18665ae46a322b6b42ec65c5dac
JuliaLui642 <159198251+JuliaLui642@users.noreply.github.com>
2024-04-26 15:14:19 -0700

    M6 L3 - L3 P1-2

--- M06.looting_workbook/practices/L3.P1.hover_the_balloon/balloon_hover.gd
+++ M06.looting_workbook/practices/L3.P1.hover_the_balloon/balloon_hover.gd
@@ -6,7 +6,8 @@ extends Area2D
 
 func _ready() -> void:
 	set_outline_thickness(5.0)
-
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
 
 func set_outline_thickness(new_thickness: float) -> void:
 	if canvas_group_has_material:
@@ -14,8 +15,9 @@ func set_outline_thickness(new_thickness: float) -> void:
 
 
 func _on_mouse_entered() -> void:
-	pass
-
+	canvas_group.material.set_shader_parameter("line_thickness", 10)
+	var tween := create_tween()
+	tween.tween_method(set_outline_thickness, 3.0, 6.0, 0.08)
 
 func _on_mouse_exited() -> void:
-	pass
+	canvas_group.material.set_shader_parameter("line_thickness", 5)
