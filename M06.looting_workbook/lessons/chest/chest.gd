a6ccd48e167f85b884344af9ab7013c75a8dc1b3
JuliaLui642 <159198251+JuliaLui642@users.noreply.github.com>
2024-04-30 14:10:46 -0700

    M6 Lesson 5 and L5.P1-2

--- M06.looting_workbook/lessons/chest/chest.gd
+++ M06.looting_workbook/lessons/chest/chest.gd
@@ -1,6 +1,16 @@
 extends Area2D
 
 @onready var canvas_group:CanvasGroup = $CanvasGroup
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+var is_open := false
+
+
+func open() -> void:
+	animation_player.play("open")
+	if is_open:
+		return
+	is_open = true
+	input_pickable = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,3 +28,14 @@ func _on_mouse_exited() -> void:
 
 func set_outline_thickness(new_thickness: float) -> void:
 	canvas_group.material.set_shader_parameter("line_thickness", new_thickness)
+
+func _input_event(viewpoint: Node, event: InputEvent, shape_index: int):
+	var event_is_mouse_click: bool = (
+		event is InputEventMouseButton and 
+		event.button_index == MOUSE_BUTTON_LEFT and 
+		event.is_pressed()
+	)
+
+	if event_is_mouse_click:
+		open()
+
--- M06.looting_workbook/practices/L5.P2.pop_the_balloon/balloon_pop.gd
+++ M06.looting_workbook/practices/L5.P2.pop_the_balloon/balloon_pop.gd
@@ -3,9 +3,10 @@ extends Area2D
 
 func _input_event(viewport: Node, event: InputEvent, shape_index: int):
 	var event_is_mouse_release: bool = (
-		event is InputEventMouseButton
-		and event.button_index == MOUSE_BUTTON_LEFT
+		event is InputEventMouseButton # false
+		and event.button_index == MOUSE_BUTTON_LEFT #
+		and event.is_released()
 	)
 
 	if event_is_mouse_release:
-		pass
+		queue_free() # pass
