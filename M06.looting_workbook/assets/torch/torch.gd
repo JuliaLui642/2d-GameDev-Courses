c0340cfc62914eb0de7850560077760342f27dc2
JuliaLui642 <JuLUI@s.sfusd.edu>
2024-05-01 15:01:27 -0700

    M6 L9 torch first half

--- M06.looting_workbook/assets/torch/torch.gd
+++ M06.looting_workbook/assets/torch/torch.gd
@@ -1,7 +1,11 @@
 extends Node2D
 
 @onready var flame: Sprite2D = $Flame
+@onready var target_node: Sprite2D = $SomeSprite2DNode
 
+func toggle_target_node_visibility() -> void:
+	target_node.visible = not target_node.visible
+	
 func _ready() -> void:
 	# This parameter of the shader material gives each flame a slightly different look and randomized animation.
 	# Can I make a commit now?
